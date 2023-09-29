8d7aff2f12248b9e5c882429c7e3199f3cbc3521
Nelson Mauricio <nelsonmauricionavarrozaraza@gmail.com>
2023-09-29 15:23:55 -0500

    Creacion del proyecto y estructura de este con MVC

---
+++ src/main/java/com/api/ufpso/tienda/model/Articulo.java
@@ -0,0 +1,10 @@
+package com.api.ufpso.tienda.model;
+
+public class Articulo {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String stock;
+    private String precio;
+    private String fechaIngreso;
+}
---
+++ src/main/java/com/api/ufpso/tienda/model/Categoria.java
@@ -0,0 +1,5 @@
+package com.api.ufpso.tienda.model;
+
+public class Categoria {
+
+}
