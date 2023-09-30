4ae18dab12cd64aeee0cdc143cc6b7c06179bec3
Nelson Mauricio <nelsonmauricionavarrozaraza@gmail.com>
2023-09-29 20:52:41 -0500

    Creacion del controller, model, repository y service de los Articulos

--- src/main/java/com/api/ufpso/tienda/model/Articulo.java
+++ src/main/java/com/api/ufpso/tienda/model/Articulo.java
@@ -1,10 +1,20 @@
 package com.api.ufpso.tienda.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Articulo {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY
+    )
     private Long id;
     private String nombre;
     private String descripcion;
     private String stock;
     private String precio;
     private String fechaIngreso;
+    @ManyToOne
+    @JoinColumn(name="categoria_id")
+    private Categoria categoria;
 }
--- src/main/java/com/api/ufpso/tienda/service/ArticuloService.java
+++ src/main/java/com/api/ufpso/tienda/service/ArticuloService.java
@@ -1,4 +1,48 @@
 package com.api.ufpso.tienda.service;
 
+import com.api.ufpso.tienda.model.Articulo;
+import com.api.ufpso.tienda.repository.ArticuloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ArticuloService {
+    @Autowired
+    private ArticuloRepository articuloRepository;
+
+    public Articulo createArticulo(Articulo articuloReq){
+        return articuloRepository.save(articuloReq);
+    }
+    public Articulo getArticuloById(Long id){
+        return articuloRepository.findById(id).get();
+    }
+
+    public Articulo updateArticulo(Articulo articuloReq, Long id){
+        Optional<Articulo> articuloBd = articuloRepository.findById(id);
+        if(articuloBd.isEmpty()){
+            return null;
+        }
+        articuloBd.get().setNombre(articuloReq.getNombre());
+        articuloBd.get().setDescripcion(articuloReq.getDescripcion());
+        articuloBd.get().setStock(articuloReq.getStock());
+        articuloBd.get().setPrecio(articuloReq.getPrecio());
+        articuloBd.get().setFechaIngreso(articuloReq.getFechaIngreso());
+        articuloBd.get().setCategoria(articuloReq.getCategoria());
+        return articuloRepository.save(articuloBd.get());
+    }
+
+    public boolean deleteArticulo(Long id){
+        Optional<Articulo> articuloBd = articuloRepository.findById(id);
+        if (articuloBd.isEmpty()){
+            return false;
+        }
+        articuloRepository.delete(articuloBd.get());
+        return true;
+    }
+
+    public List<Articulo> findAllArticulo(){return (List<Articulo>) articuloRepository.findAll();}
 }
