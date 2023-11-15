8db7d7380792891ea0facb117c39851d33a10f6b
Nelson Zaraza <114716118+NelsonZarazaDev@users.noreply.github.com>
2023-11-14 19:13:04 -0500

    Commit Lemus

--- src/main/java/com/api/ufpso/tienda/auth/TokenUtils.java
+++ src/main/java/com/api/ufpso/tienda/auth/TokenUtils.java
@@ -9,7 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import java.util.*;
 
 public class TokenUtils {
-    private final static String ACCESS_TOKEN_SECRET = "XqR8s2uPb3WmG9jL5kN0yZa1OcV7f";
+    private final static String ACCESS_TOKEN_SECRET = "4qhq8LrEBfYcaRHxhdb9zURb2rf87Ud";
     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 
     public static String createToken(String nombre, String email) {
